a0f102ed6378075dd214d9659177241b9adaac9e
dylannaiken <dylannaiken06@gmail.com>
2025-02-15 15:25:13 +0400

    modif model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,22 @@
 class Food < ApplicationRecord
   belongs_to :meal
   has_one :nutrition, dependent: :destroy
+  has_one_attached :photo
+
+  after_save :update_daily_log_remaining_calories
+  after_destroy :update_daily_log_remaining_calories
+
+  # enum serving_unit: {
+  #   grams: 'grams'
+  #   liters: 'liters'
+  #   cups: 'cups'
+  #   tablespoons: 'tablespoons'
+  #   teaspoons: 'teaspoons'
+  # }
+
+  private 
+
+  def update_daily_log_remaining_calories
+    meal.daily_log.update_remaining_calories
+  end 
 end
